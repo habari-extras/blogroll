3cc3a1b7e66181a284707fb947e7ae7a1588487d
dmondark <USER@DOMAIN>
2008-04-06 19:28:37 +0000

    plugin:blogroll Fixed the "Undefined variable" error, use empty instead of the var alone.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/blogroll@125 f4a90942-1786-4aa1-b092-57c31935ddc3

--- templates/blogroll.php
+++ templates/blogroll.php
@@ -1,7 +1,7 @@
 <!-- To customize this template, copy it to your currently active theme directory and edit it -->
 <div id="blogroll">
 	<ul>
-	<?php if ( $blogs ) { foreach( $blogs as $blog ) { ?>
+	<?php if ( !empty( $blogs ) ) { foreach( $blogs as $blog ) { ?>
 		<li><a href="<?php echo $blog->url; ?>"><?php echo $blog->name; ?></a></li>
 	<?php } } ?>
 	</ul>
