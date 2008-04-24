0637ed0f2ac519c1cbfb9852ef5805cc12e2c8c4
miklb <USER@DOMAIN>
2008-04-24 13:52:04 +0000

    Plugin:Blogroll Adding list title from options to blogroll template
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/blogroll@150 f4a90942-1786-4aa1-b092-57c31935ddc3

--- templates/blogroll.php
+++ templates/blogroll.php
@@ -1,5 +1,6 @@
 <!-- To customize this template, copy it to your currently active theme directory and edit it -->
 <div id="blogroll">
+	<h2><?php echo Options::get( 'blogroll:list_title' ); ?></h2>
 	<ul>
 	<?php if ( ! empty( $blogs ) ) { foreach( $blogs as $blog ) { ?>
 		<li><a href="<?php echo $blog->url; ?>"><?php echo $blog->name; ?></a></li>
