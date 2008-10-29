3763acdeb842928d351515d61913c2b25a943127
MattRead <USER@DOMAIN>
2008-10-29 19:31:38 +0000

    I luv turtles
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/blogroll/trunk@1223 f4a90942-1786-4aa1-b092-57c31935ddc3

--- templates/blogroll.php
+++ templates/blogroll.php
@@ -1,6 +1,6 @@
 <!-- To customize this template, copy it to your currently active theme directory and edit it -->
 <div id="blogroll">
-	<h2><?php echo Options::get( 'blogroll__list_title' ); ?></h2>
+	<h2><?php echo $blogroll_title; ?></h2>
 	<ul>
 	<?php if ( ! empty( $blogs ) ) { foreach( $blogs as $blog ) { ?>
 		<li><a href="<?php echo $blog->info->url; ?>"><?php echo $blog->title; ?></a></li>
